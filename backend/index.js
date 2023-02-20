8177fcae8cb8f11ad652c029388c08ae1960a86e
amitk161 <a.kamitkumar1920@gmail.com>
2023-02-20 21:41:23 +0530

    Deployment Changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || process.env.PORTNO;
 const frontURL = process.env.FRONT_URL;
 const mongoDB = require("./db");
 mongoDB();
