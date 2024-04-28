3eb7d43bd51c760eede66bdb58356bf80e5bf424
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-04-28 13:39:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || process.env.PORTNO;
 const frontURL = process.env.FRONT_URL;
 const mongoDB = require("./db");
