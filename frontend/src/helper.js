c991db085458e11b403fb95f54739780e47773ce
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-04-30 09:54:29 +0530

    Update helper.js

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,3 +1,3 @@
 //export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "https://foodmaniabackend.onrender.com";
-export const BASE_URL = "https://food-mania-seven.vercel.app"
+export const BASE_URL = "https://food-mania-backend.vercel.app"
